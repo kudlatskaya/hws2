17d9192a6fe82b345d91e8c2a33e4e904b90eda9
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-03-09 15:56:07 +0300

    hw1: layout added

--- src/s2-homeworks/hw01/HW1.tsx
+++ src/s2-homeworks/hw01/HW1.tsx
@@ -53,7 +53,8 @@ export const friendMessage0: MessageType = {
 const HW1 = () => {
     return (
         <div id={'hw1'}>
-            <div className={s2.hwTitle}>Homework #1</div>
+            <div className={s2.hwTitle}>Hometask #1</div>
+            <hr />
             <div className={s2.hw}>
                 {/*проверка отображения (не менять)*/}
                 <div>
