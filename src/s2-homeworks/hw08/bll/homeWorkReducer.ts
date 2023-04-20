0468014f6260b76d616c230388cba011a069c744
Marina Kudlatskaya <m.kudlatskaya@gmail.com>
2023-04-20 16:48:09 +0300

    hw8 bugs fixed1

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,31 +7,33 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-            // в алфавитном порядке a.name > b.name
+
             if(action.payload === 'up') {
-                return [...state.sort((a, b) => {
+
+                let _state = [...state]
+                return _state.sort((a, b) => {
                     let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
-                    if (nameA < nameB) //сортируем строки по возрастанию
-                        return -1
-                    if (nameA > nameB)
-                        return 1
+                    if (nameA < nameB) return -1 //сортируем строки по возрастанию
+                    if (nameA > nameB) return 1
                     return 0 // Никакой сортировки
-                } )]
+                } )
             }
-            // в обратном порядке a.name < b.name}
+
             if(action.payload === 'down') {
-                return [...state.sort((a, b) => {
+
+                let _state = [...state]
+                return _state.sort((a, b) => {
                     let nameA = a.name.toLowerCase(), nameB = b.name.toLowerCase()
                     if (nameA < nameB) return 1
                     if (nameA > nameB) return -1
                     return 0
-                } )]
+                } )
             }
             return state // need to fix
         }
         case 'check': {
 
-            return [...state.filter((u) => u.age > action.payload)] // need to fix
+            return [...state.filter((u) => u.age > action.payload)]// need to fix
         }
         default:
             return state
